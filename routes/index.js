c26a80353db7ffcdecb6998e0537eb5edb66540f
Obinna Joseph <binazajoe@gmail.com>
2023-04-25 00:53:40 +0300

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Obyno learning Devops from scratch' });
+  res.render('index', { title: 'Obyno Obyno Obyno Obyno' });
 });
 
 module.exports = router;
